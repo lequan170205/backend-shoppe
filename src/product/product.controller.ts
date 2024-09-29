90b5f6721e2299091d1834a78f5df65edb5a2fb4
HieuCuteDangYeu <nguyendinhhieu050505@gmail.com>
2024-09-29 16:02:45 +0700

    Update product.controller.ts

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -8,13 +8,11 @@ import { CreateProductDto } from "./dto/create-product.dto";
 export class ProductController {
     constructor(private readonly productService: ProductService) {}
 
-    @UseGuards(JwtAuthGuard)
     @Post('find')
     async getAllProducts() {
         return this.productService.findAllProducts();
     }
 
-    @UseGuards(JwtAuthGuard)
     @Post('add')
     async addProduct(@Body() createProductDto: CreateProductDto): Promise<Product> {
         return this.productService.addProduct(createProductDto);
