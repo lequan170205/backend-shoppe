904b15f88731f1baa7558100420035eca60a9d4e
Fink2005 <phans3806@gmail.com>
2024-09-30 22:13:05 +0700

    update findId and update

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, UseGuards, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  UseGuards,
+  Post,
+  Get,
+  Param,
+  Patch,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
-import { Product } from './schema/product.schema';
+import { Product } from './schemas/product.schema';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductController {
@@ -20,5 +31,23 @@ export class ProductController {
     @Body() createProductDto: CreateProductDto,
   ): Promise<Product> {
     return this.productService.addProduct(createProductDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('find/:id')
+  async findProductById(@Param('id') id: string) {
+    const findProductId = await this.productService.findProductById(id);
+    return findProductId;
+  }
+
+
+  
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  updateProductById(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productService.updateProductById(id, updateProductDto);
   }
 }
