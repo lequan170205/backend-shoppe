05320593666ef2a5a4a0d81b49fc0958e269aaaf
Fink2005 <phans3806@gmail.com>
2024-10-07 15:20:21 +0700

    sy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:8080', // Địa chỉ frontend của bạn
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Các phương thức HTTP được phép
+    allowedHeaders: 'Content-Type,Authorization', // Các header được phép
+  });
   await app.listen(3000);
 }
 bootstrap();
